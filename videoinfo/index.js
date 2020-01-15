7dd36a2c18e0659c184b73bc7f9687552fba8421
Deivid <davidherrero96@gmail.com>
2020-01-15 11:00:24 +0100

    Finalizada la aplicación

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,8 +1,25 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron
+let mainWindow;
+
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
     mainWindow.loadURL(`file://${__dirname}/index.html`)
+});
+
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        //console.log('Video duration is: ', metadata.format.duration);
+        mainWindow.webContents.send('video:metadata', metadata.format.duration);
+    });
+    
+
+
 });
